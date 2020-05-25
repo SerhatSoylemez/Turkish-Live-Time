6c58b5d3491222dcf782945c331fbe1b63718528
Serhat Soylemez <serhatsoylemez@live.com>
2020-05-25 13:08:06 +0300

    Create showtimetr.php

---
+++ showtimetr.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="tr" >
+<head>
+<meta charset="utf-8" />
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+var otomatik_yenile = setInterval(
+  function() {
+    $('#yenile').load('livetimetr.php').fadeIn('slow');
+  }, 1000
+); 
+</script>
+
+</head>
+<body>
+
+<?php
+echo '<div id="yenile"></div>'
+?>
+
+</body>
+</html>
